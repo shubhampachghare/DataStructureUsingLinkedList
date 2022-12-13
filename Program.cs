fe1eb1c1b8c87a83d3fcaeaed233aa9f722d428e
shubhampachghare <shubhampachghare@gmail.com>
2022-12-14 01:49:42 +0530

    [Shubham]-Created the linked list of 56,30,70

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace DataStructure
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList linkedList1= new LinkedList();
+            linkedList1.Add(56);
+            linkedList1.Add(30);
+            linkedList1.Add(70);
+        }
+    }
+}
